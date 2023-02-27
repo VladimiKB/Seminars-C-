7ab9038c8e95f0d32c3dbf9aa49f0957b030dd9d
Vladimir Badulenko <vkb030@gmail.com>
2023-02-27 23:02:56 +0700

    Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8.
+// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int divTwo = 2;
+if(number > 1)
+{
+    while(divTwo <= number)
+    {
+        Console.Write(divTwo + " ");
+        divTwo = divTwo + 2;
+    }
+}
