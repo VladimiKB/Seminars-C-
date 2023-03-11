7ea7410f8b45e75c24ac391e3c82e08d70a143f0
Vladimir Badulenko <vkb030@gmail.com>
2023-03-11 11:45:22 +0700

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 26.
+// Напишите программу, которая:
+// 1. Принимает на вход число.
+// 2. Выдаёт количество цифр в числе.
+// Например:
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digitNumbers = DigitNumbers(number);
+Console.WriteLine($"Количество цифр в числе {number} = {digitNumbers}");
+
+int DigitNumbers(int num)
+{
+    int digits = 0;
+    while (num > 0)
+    {
+       num /= 10;
+       digits++; 
+    }
+    return digits;
+}
