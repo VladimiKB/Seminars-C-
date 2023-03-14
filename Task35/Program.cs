28d0f00d23b845d84693e3386f8aa92a5beae415
Vladimir Badulenko <vkb030@gmail.com>
2023-03-14 21:13:44 +0700

    Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 35.
+// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов.
+// В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random { };
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void ArrayPrinted(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountNumberTwoDigit(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99) count++;
+    }
+    return count;
+}
+
+int[] array = CreateArrayRndInt(5, -100, 100);
+ArrayPrinted(array);
+
+int counter = CountNumberTwoDigit(array);
+Console.WriteLine(counter);
