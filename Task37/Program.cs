e2e6f11fe58b2d401ab03c187772ce2b5bdc8d5d
Vladimir Badulenko <vkb030@gmail.com>
2023-03-14 22:37:55 +0700

    Task37

--- Task37/Program.cs
+++ Task37/Program.cs
@@ -6,3 +6,43 @@
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random { };
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void ArrayPrinted(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] MultiplicationPairNumbers(int[] arr)
+{
+    int size = arr.Length / 2;
+    if (arr.Length % 2 != 0) size = size + 1;
+    int[] newArr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        newArr[i] = arr[i] * arr[arr.Length - 1 - i];
+    }
+    if (arr.Length % 2 != 0) newArr[size - 1] = arr[arr.Length/ 2];
+    return newArr;
+}
+
+int[] array = CreateArrayRndInt(5, 1, 10);
+ArrayPrinted(array);
+
+int[] newArray = MultiplicationPairNumbers(array);
+ArrayPrinted(newArray);
