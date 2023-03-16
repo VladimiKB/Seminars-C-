d2afc74039e19c171fbbc8b6cbff982bbd895f57
Vladimir Badulenko <vkb030@gmail.com>
2023-03-16 20:32:23 +0700

    Task42

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -6,3 +6,26 @@
 // 3 -> 11
 // 2 -> 10
 
+int num = Prompt("Введите целое десятичное число: ");
+int found = Prompt("Введите целое десятичное число - основание системы исчесления: ");
+
+Console.WriteLine(DecToBin(num, found));
+
+int DecToBin(int number, int foundation)
+{
+    int res = 0;
+    int multiplier = 1;
+    while (number > 0)
+    {
+        res += (number % foundation) * multiplier;
+        number = number / foundation;
+        multiplier *= 10;
+    }
+    return res;
+}
+
+int Prompt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
