4046a00d2676d9e63131c2b6f5788162c8d33c8d
Vladimir Badulenko <vkb030@gmail.com>
2023-03-23 20:00:27 +0700

    Task51

--- Task51/Program.cs
+++ Task51/Program.cs
@@ -6,3 +6,63 @@
 // 8 4 2 4
 // Сумма элементов главной диагонали: 1+9+2 = 12
 
+Console.Clear();
+
+int row = Prompt($"Введите количество срок: ");
+int col = Prompt($"Введите количество столбцов: ");
+int startValue = Prompt($"Введите начало диапазона случайных чисел массива: ");
+int endValue = Prompt($"Введите конец диапазона случайных чисел массива: ");
+
+int MatrixDiagonalSum(int[,] matrix)
+{
+    int sum = 0;
+    // int size = matrix.GetLength(0);
+    // if (matrix.GetLength(1) < size)
+    // {
+    //     size = matrix.GetLength(1);
+    // }
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+    {
+        sum += matrix[i, i];
+    }
+    return sum;
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random { };
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(row, col, startValue, endValue);
+PrintMatrix(array2d);
+
+int matrixDiagonalSum = MatrixDiagonalSum(array2d);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов главной диагонали: {matrixDiagonalSum}");
+
+int Prompt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
