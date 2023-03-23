55d3e7a841577e5c56f810a291bc9506a180468d
Vladimir Badulenko <vkb030@gmail.com>
2023-03-23 18:46:45 +0700

    Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 49.
+// Задайте двумерный массив.
+// Найдите элементы, у которых оба индекса чётные,
+// и замените эти элементы на их квадраты.
+
+// Например, изначально массив выглядел вот так:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Новый массив будет выглядеть вот так:
+
+//  {1}  4  {49}  2
+//   5   9    2   3
+// {64}  4   {4}  4
+
+Console.Clear();  // автоматическая очистка окна терминала
+
+int row = Prompt($"Введите количество срок: ");
+int col = Prompt($"Введите количество столбцов: ");
+int startValue = Prompt($"Введите начало диапазона случайных чисел массива: ");
+int endValue = Prompt($"Введите конец диапазона случайных чисел массива: ");
+
+void SquareIndexesEven(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random { };
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(row, col, startValue, endValue);
+PrintMatrix(array2d);
+SquareIndexesEven(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
+
+int Prompt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
